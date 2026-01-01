d407f1ba83a3c77754edac706d3164c576ff56a9
Gabriel plahtyn <gabrielplahtyn@gmail.com>
2026-01-01 15:40:38 -0300

    carrocel1

--- portifoliogabe/script.js
+++ portifoliogabe/script.js
@@ -1,38 +1,34 @@
 const slider = document.querySelectorAll('.sliderImg');
+const texts = document.querySelectorAll('.sliderText');
 const btnPrev = document.getElementById("prevButon");
 const btnNext = document.getElementById("nextButon");
 
 let currentSlide = 0;
 
-function hideSlider(){
-    slider.forEach(item => item.classList.remove('on'))
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('on'));
+  texts.forEach(item => item.classList.remove('on'));
 }
 
-function showSloder(){
-    slider[currentSlide].classList.add('on')
+function showSlider() {
+  slider[currentSlide].classList.add('on');
+  texts[currentSlide].classList.add('on');
 }
 
+// mostrar o primeiro slide e texto ao carregar
+showSlider();
 
-function nextSlide(){
-    hideSlider()
-    if(currentSlide == slider.length - 1){
-         currentSlide = 0
-    } else {
-        currentSlide ++
-    }
+function nextSlide() {
+  hideSlider();
+  currentSlide = (currentSlide === slider.length - 1) ? 0 : currentSlide + 1;
+  showSlider();
 }
 
-function prevSlide(){
-    hideSlider()
-    if(currentSlide == 0){
-         currentSlide = slider.length -1 
-    } else {
-        currentSlide--
-    }
+function prevSlide() {
+  hideSlider();
+  currentSlide = (currentSlide === 0) ? slider.length - 1 : currentSlide - 1;
+  showSlider();
 }
-btnNext.addEventListener('click', nextSlide)
-btnPrev.addEventListener('click', prevSlide)
 
-
-console.log(slider)
-
+btnNext.addEventListener('click', nextSlide);
+btnPrev.addEventListener('click', prevSlide);
